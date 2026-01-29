7ebb548049660f517882c4988a1c8c38adebc5f4
cubeee <cubeee.gh@gmail.com>
2026-01-30 00:08:33 +0200

    Add more text to mapped star location matching

--- lib/stars.go
+++ lib/stars.go
@@ -14,12 +14,12 @@ var StarLocations map[string]StarLocation
 // TODO: load from json?
 func init() {
 	StarLocations = make(map[string]StarLocation)
-	StarLocations["south east"] = StarLocation{X: 1745, Y: 2954}
+	StarLocations["south east mine"] = StarLocation{X: 1745, Y: 2954}
 	StarLocations["hunter guild"] = StarLocation{X: 1487, Y: 3090}
-	StarLocations["colosseum"] = StarLocation{X: 1773, Y: 3102}
+	StarLocations["colosseum entrance"] = StarLocation{X: 1773, Y: 3102}
 	StarLocations["salvager overlook"] = StarLocation{X: 1627, Y: 3275}
 	StarLocations["aldarin"] = StarLocation{X: 1422, Y: 2874}
-	StarLocations["custodia"] = StarLocation{X: 1290, Y: 3411}
+	StarLocations["custodia mountains"] = StarLocation{X: 1290, Y: 3411}
 }
 
 func GetStarLocation(calledLocation string) *StarLocation {
